16af780003aa3324f1e54749dfc76ba4bf9832af
s182843 <56959419+s182843@users.noreply.github.com>
2019-10-27 00:53:37 +0200

    uppdate
    uppdate

--- Individuell Webbdesign/html5-boilerplate_v7.2.0/js/main2.js
+++ Individuell Webbdesign/html5-boilerplate_v7.2.0/js/main2.js
@@ -1,10 +1,12 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -14,42 +16,12 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
+  }
+  for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
-    }
+  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active"; 
 }
-
-
-
-
-
-
-/* sidebar
-
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-} */
-
-
-
-
-
-
-
-
-
-
-
-
