4b076be512386a3d21009c3bfb296be80bd1dd0f
s182843 <56959419+s182843@users.noreply.github.com>
2019-10-26 16:43:09 +0200

    Uppdate
    Uppdate

--- Individuell Webbdesign/html5-boilerplate_v7.2.0/js/main.js
+++ Individuell Webbdesign/html5-boilerplate_v7.2.0/js/main.js
@@ -24,3 +24,20 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+
+
+
+
+/* sidebar
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+} */
